980079afd826041f7bed288d13998514376b0725
guille <guillermocarrasco2a@gmail.com>
2022-11-26 23:05:21 -0300

    version 8

--- src/main/java/com/portafolio/helmet/repositories/EstadoAsesoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portafolio.helmet.repositories;
-
-import com.portafolio.helmet.entities.EstadoAsesoria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EstadoAsesoriaRepository extends CrudRepository<EstadoAsesoria, Long> {
-}
